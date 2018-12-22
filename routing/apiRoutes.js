81e03474a4e213431a766c40fd92c09a9615aef8
Laurie Anderson <plipandfirt@gmail.com>
2018-12-22 12:10:35 -0500

    try to figure out routing files - what goes where

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -2,21 +2,21 @@
 
 var friendsData = require("../data/friendsData");
 
-//routing
+//honestly, I don't understand what requests go in what files. I know putting all of them in every file isn't correct. Right now I have app.xxx requests in 3 files...very frustrating.  :`(
+
+app.get("/api/friends", function (req, res) {
+    res.json(friendsData);
+});
+
+app.post("/api/friends", function (req, res) {
+    if (friendsData.length < 5) {
+        friendsData.push(req.body);
+        res.json(true);
+    }
+});
+
+app.post("/api/friends", function (req, res) {
+    tableData.length = [];
+});
 
 module.exports = function (app) {
-    app.get("/api/friends", function (req, res) {
-        res.json(friendsData);
-    });
-
-    // app.post("/api/friends", function (req, res) {
-    //     if (friendsData.length < 5) {
-    //         friendsData.push(req.body);
-    //         res.json(true);
-    //     }
-    // });
-
-    app.post("/api/friends", function (req, res) {
-        tableData.length = [];
-    });
-};
