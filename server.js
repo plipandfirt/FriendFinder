7ecc8c6ce8742e4cb1da05dc01553f358187fd2b
Laurie Anderson <plipandfirt@gmail.com>
2018-12-22 12:46:29 -0500

    typo

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ require("./routing/htmlRoutes")(app);
 // Routes
 // =============================================================
 
-// Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "app/public/home.html"));
   });
